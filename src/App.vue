90fdec790f744ce80cce8020d4f99950cd2c29f5
unknown <jordanrob709@gmail.com>
2021-11-14 20:23:27 +0300

    added custom event to pass data from child to parent component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
 </template>
 
 <script>
@@ -12,7 +12,9 @@ export default {
   data(){
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   components: {
@@ -22,6 +24,13 @@ export default {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
+
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -27,7 +27,7 @@ export default {
         },
         stopTimer(){
             clearInterval(this.timer)
-            console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime)
         }
     }
     /*

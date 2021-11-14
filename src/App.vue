5253143f379fc1c47b195bc0a405d941548b9320
unknown <jordanrob709@gmail.com>
2021-11-14 18:55:48 +0300

    implemented block component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
   components: {
+    Block
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
   }
 }
 </script>

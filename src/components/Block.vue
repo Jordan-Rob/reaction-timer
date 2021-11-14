48c4b2eeecd15322462cd079cf44272dace40b1a
unknown <jordanrob709@gmail.com>
2021-11-14 19:40:01 +0300

    added delay using component lifecycle hook

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="block">
+    <div class="block" v-if="showBlock">
         Click me
     </div>
 </template>
 <script>
 export default {
-    props:['delay',]
+    props:['delay',],
+    data(){
+        return{
+            showBlock: false
+        }
+    },
+    mounted(){
+        console.log('component mounted')
+        setTimeout(()=>{
+            this.showBlock = true
+            console.log(this.delay)
+        }, this.delay)
+    },
+    updated(){
+        console.log('component updated')
+    },
+    unmounted(){
+        console.log('component unmounted')
+    }
 }
 </script>
 <style>
